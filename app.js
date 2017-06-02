16d58444a10ece2e3056ccff20e68b4dfc54f454
Gunpreet Singh <gunpreet@outlook.com>
2017-06-02 15:26:54 -0700

    added linter. basic function

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// TODO
+class ShoppingListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onClick() {
+    if (this.state.done) {
+      this.setState({
+        done: false
+      });
+    } else {
+      this.setState({
+        done: true
+      });
+    }
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'bold'
+    };
+    return (
+      <li
+        style={style}
+        onClick={this.onClick.bind(this)}
+      >
+        {this.props.item}
+      </li>
+    );
+  }
+}
+
+var List = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <ShoppingListItem item={item} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<List items={['Kale', 'Cucumber', 'Onions']} />, document.getElementById('app'));
